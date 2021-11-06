2a1d102f0e095d0ffb135bbfb9c661b740668e83
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2021-11-06 15:37:29 -0600

    Uncontrolled form con Ref React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-function App() {
+import { useRef } from "react";
 
-  const submit = e => {
-    e.preventDefault();
-    const dataForm = Array.from(new FormData(e.target));
-    const objData = Object.fromEntries(dataForm);
+const App = () => {
+  const input1 = useRef();
+  const input2 = useRef();
+  const file = useRef();
 
-    console.log(objData);
+  const submit = () => {
+    const campo1 = input1.current.value;
+    const campo2 = input2.current.value;
+    const archivo = file.current.files[0];
+
+    const form = new FormData();
+    form.append('campo1', campo1);
+    form.append('campo2', campo2);
+    form.append('archivo', archivo);
+
+    fetch('/test', { method: 'POST', body: form });
   };
 
   return (
-    <form onSubmit={submit}>
+    <div>
       <div>
-        <span>campo</span>
-        <input name="campo" />
+        <span>campo1</span>
+        <input
+          type="text"
+          name="campo1"
+          ref={input1}
+        />
       </div>
 
-      <input name="campo2" />
+      <input
+        type="text"
+        name="campo2"
+        ref={input2}
+      />
 
-      <input type="submit" value="Enviar" />
-    </form>
+      <input type="file" ref={file} />
+
+      <input type="submit" value="Enviar" onClick={submit} />
+    </div>
   );
-}
+};
 
 export default App;
---
+++ src/AppUncontrolledHtml.js
@@ -0,0 +1,25 @@
+function App() {
+
+  const submit = e => {
+    e.preventDefault();
+    const dataForm = Array.from(new FormData(e.target));
+    const objData = Object.fromEntries(dataForm);
+
+    console.log(objData);
+  };
+
+  return (
+    <form onSubmit={submit}>
+      <div>
+        <span>campo</span>
+        <input name="campo" />
+      </div>
+
+      <input name="campo2" />
+
+      <input type="submit" value="Enviar" />
+    </form>
+  );
+}
+
+export default App;
