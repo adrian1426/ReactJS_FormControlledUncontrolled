cd6590f16f5a4a525331460656a75e45b5c9b69d
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2021-11-06 17:49:25 -0600

    Controlando select,checkbox y radiobuttons

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import { useState } from 'react';
 
 const initialState = {
   normal: '',
-  texto: ''
+  texto: '',
+  lenguaje: '',
+  check: false,
+  estado: 'feliz'
 };
 
 const App = () => {
   const [value, setValue] = useState(initialState);
 
   const handleChange = e => {
+    const { name, type, checked, value: valor } = e.target;
     setValue({
       ...value,
-      [e.target.name]: e.target.value
+      [name]: type === 'checkbox' ? checked : valor
     });
   };
 
@@ -29,6 +33,48 @@ const App = () => {
         value={value.texto}
         onChange={handleChange}
       />
+
+      <select value={value.lenguaje} name='lenguaje' onChange={handleChange}>
+        <option value=''>Seleccionar</option>
+        <option value='react'>React JS</option>
+        <option value='angula'>Angula</option>
+        <option value='vue'>VUE</option>
+      </select>
+
+      <input
+        type='checkbox'
+        name='check'
+        onChange={handleChange}
+        checked={value.check}
+      />
+
+      <div>
+        <label>Chancho</label>
+        <input
+          onChange={handleChange}
+          type='radio'
+          value='feliz'
+          name='estado'
+          checked={value.estado === 'feliz'}
+        /> Feliz
+
+        <input
+          onChange={handleChange}
+          type='radio'
+          value='triste'
+          name='estado'
+          checked={value.estado === 'triste'}
+        /> Triste
+
+        <input
+          onChange={handleChange}
+          type='radio'
+          value='enojado'
+          name='estado'
+          checked={value.estado === 'enojado'}
+        /> Enojado
+
+      </div>
     </div>
   );
 };
